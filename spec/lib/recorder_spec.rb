8bc0381455be7290baf50d57936cdc829282869f
Benjamin Fleischer <github@benjaminfleischer.com>
2015-05-18 11:23:51 -0500

    Refactor test for expectation clarity

--- spec/lib/recorder_spec.rb
+++ spec/lib/recorder_spec.rb
@@ -1,26 +1,31 @@
 RSpec.describe Recorder, type: :model do
+  DELIMITERS =
+    { pipes: "|", commas: ",", spaces: " " }
+  let(:header_row) {
+    %w[LastName FirstName Gender FavoriteColor DateOfBirth]
+  }
+  let(:data_row) {
+    %w[Clinton Bill Male Blue 2015-05-18]
+  }
+  let(:headers) {
+    %i[lastname firstname gender favoritecolor dateofbirth]
+  }
   describe "parses a record delimited by" do
-    { pipes: "|", commas: ",", spaces: " " }.each do |name, delimiter|
-      it "#{name}: '#{delimiter}' delimited" do
-        record = build_rows([
-          %w[LastName FirstName Gender FavoriteColor DateOfBirth],
-          %w[Clinton Bill Male Blue 2015-05-18],
-        ], delimiter: delimiter)
+    DELIMITERS.each do |name, delimiter|
+      # TODO: extract into shared example
+      specify "#{name}: '#{delimiter}' delimited" do
+        record = build_rows([header_row, data_row], delimiter: delimiter)
 
         actual = Recorder.parse(record)
+        # sanity check
         expect(actual.size).to eq(1)
 
-        expected = [
-          "Clinton",
-          "Bill",
-          "Male",
-          "Blue",
-          Date.new(2015, 5, 18),
-        ]
+        expected = data_row[0..-2] << Date.new(2015, 5, 18)
         first_row = actual.first
-        headers = [:lastname, :firstname, :gender, :favoritecolor, :dateofbirth]
 
+        # confirm rows parsed by header
         expect(first_row.values_at(*headers)).to eq(expected)
+        # confirm date field parsed
         expect(first_row[:dateofbirth]).to be_a(Date)
       end
     end
