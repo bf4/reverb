7daff7de785209825bfed8236fb0ee5904a94441
Benjamin Fleischer <github@benjaminfleischer.com>
2015-05-20 13:49:11 -0500

    Test basic record creation over API

--- lib/recorder/api.rb
+++ lib/recorder/api.rb
@@ -1,6 +1,58 @@
+#  curl \
+#    -H "Accept-Version:v1" \
+#    -H Accept:application/json \
+#    -X POST \
+#    -F "delimited_record=@spec/fixtures/record.csv" \
+#    http://localhost:9292/api/records
 require "grape"
 require "grape-roar"
 module Recorder
   class API < Grape::API
+    version "v1", using: :accept_version_header
+    format :json
+    prefix :api
+
+    helpers do
+      def builder
+        @builder ||= Recorder::Builder.new
+      end
+
+      # @return [String] csv representation of table
+      def json_table(table)
+        table.to_csv.chomp
+      end
+
+      def table_from_params(params)
+        errors = []
+        delimited_record = params[:delimited_record]
+        record = delimited_record["filename"] ? delimited_record.tempfile : delimited_record
+        table = builder.parse(record).last
+      rescue RuntimeError => e
+        if e.message =~ /delimiter/
+          errors << e.message
+        else
+          raise
+        end
+      ensure
+        return [table, errors]
+      end
+    end
+
+    resources :records do
+      desc "Post a single data line in any of the supported formats"
+      params do
+        requires :delimited_record, desc: "The delimited record."
+      end
+      post do
+        table, errors = table_from_params(params)
+        if errors.empty?
+          status 201
+          { data: json_table(table) }
+        else
+          status 422
+          { errors: {status: 422, title: "Could not parse record", messages: errors } }
+        end
+      end
+    end
   end
 end
--- spec/lib/recorder/api_spec.rb
+++ spec/lib/recorder/api_spec.rb
@@ -6,10 +6,54 @@ RSpec.describe Recorder::API, type: :web do
      Recorder::API
    end
 
-   describe "POST /records"
-   # Post a single data line in any of the 3 formats supported by your existing code
+   describe "POST /records" do
+     let(:delimited_record) do
+       "lastname,firstname,gender,favoritecolor,dateofbirth\n"\
+       "Last,Woman,Female,Venetian,2000-09-30".freeze
+     end
+
+     specify "a single line of data" do
+       create_params = {delimited_record: delimited_record}
+       post "/api/records", create_params
+       expect(status_code).to eq(201)
+       expect(response_body).to eq(
+         {
+           "data" => delimited_record,
+         }
+       )
+     end
+
+     it "returns a 422 error when the table is not parseable" do
+       create_params = {delimited_record: "foo;bar"}
+       post "/api/records", create_params
+       expect(status_code).to eq(422)
+       expect(response_body).to eq(
+         {
+           "errors" => {
+             "status" => 422,
+             "title" => "Could not parse record",
+             "messages" => [
+               "No delimiter found for foo;bar"
+             ]
+           }
+         }
+       )
+     end
+   end
    describe "GET /records/gender" # Output 1
    describe "GET /records/birthdate" # Output 2
    describe "GET /records/name" # Output 3
+
+   def response
+     last_response
+   end
+
+   def status_code
+     response.status
+   end
+
+   def response_body
+     JSON.load(response.body)
+   end
 
 end
